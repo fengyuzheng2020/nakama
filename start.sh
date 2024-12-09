594b2e1523c3f4512de26fbf5ebc09451194b644
freez <freez@89trillion.com>
2024-12-09 21:33:40 +0800

    添加build start脚本

--- start.sh
+++ start.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+# 初始化数据库
+#nakama migrate up --database.address "root@8.155.3.46:26257"
 echo "Starting Nakama script"
-./nakama --config local.yml > ./log/nakama.log 2>&1 &
+./nakama --config local.yml > ../logs/nakama.log 2>&1 &
 
 echo "Nakama script finished"
